1f1b95cdba9f28979f5ade1ab5415c3e95ef48cc
Gundo <sifhufhisg@gmail.com>
2023-02-21 20:37:43 +0200

    Update Watson.py

--- Watson.py
+++ Watson.py
@@ -9,22 +9,13 @@ import time
 
 
 # Authenticate with Twitter
-auth = tweepy.OAuthHandler("wnokBdU7T8LfzGujMzNwAB4MD", "KBg3A6b74pMULd6ekFr4rpQa4tKvrgTsPtN6Nk4ArMX5smuOZp")
-auth.set_access_token("2393199921-aVemIxWAOOuiqTmYAlfMkwglfsGEorMFJsfJOOH", "W3S0AOPPpELwPZWfMU5Bt6T0sbEfkAe0ScOZpY1Jbt1kh")
+auth = tweepy.OAuthHandler(SECRET_KEY, YOUR_TOKEN)
+auth.set_access_token(ACCESS_TOKEN, YOUR_TWITTER_SECRET)
 
 # Create API object
 api = tweepy.API(auth)
-#
-#{
-#  "apikey": "j_9evpvoUMosyLMVc1C25PHzh-g-lytfYFu5jUcDWfQC",
-#  "iam_apikey_description": "Auto-generated for key crn:v1:bluemix:public:natural-language-understanding:us-south:a/16c1b95d14ce4f62ad8acf7e058d8739:fb078823-bfe8-46ef-95d1-fcea867efe13:resource-key:b849596c-1180-4b13-b569-8799e8bdbdd7",
-#  "iam_apikey_name": "Auto-generated service credentials",
-#  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
-#  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/16c1b95d14ce4f62ad8acf7e058d8739::serviceid:ServiceId-5ebdadcf-6055-41b2-9588-c8402c1351dc",
-#  "url": "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/fb078823-bfe8-46ef-95d1-fcea867efe13"
-#}
 # Authenticate with IBM Watson
-authenticator = IAMAuthenticator('ZNzVubIo5QrXuXSD9rp1FyRYC0Xx_5mNyDEi-kR9S-TX')
+authenticator = IAMAuthenticator(YOUR_KEY)
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2022-04-07',
     authenticator=authenticator
